3ec97b8e7d9ffabca5159f04d4e4a5a34c23a468
polytom868-pixel <polytom868-pixel@users.noreply.github.com>
2026-06-28 05:42:36 +0100

    chore: delete unused package init files, update importers

--- siglab/live/__init__.py
+++
@@ -1,6 +0,0 @@
-from siglab.live.exporter import LiveDeploymentManager, deployment_readiness
-
-__all__ = [
-    "LiveDeploymentManager",
-    "deployment_readiness",
-]
--- siglab/operator/__init__.py
+++
@@ -1,5 +0,0 @@
-"""SigLab Operator Module."""
-
-from siglab.operator.pipeline import OperatorPipeline, Position, RiskReport, TradeSignal
-
-__all__ = ["OperatorPipeline", "Position", "RiskReport", "TradeSignal"]
